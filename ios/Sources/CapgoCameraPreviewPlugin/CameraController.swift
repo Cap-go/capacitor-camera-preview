b4071d1bd6157b71e79082447eb81016f6b0c263
buhleryesco <sbuhler@yesco.com>
2026-03-23 15:43:43 -0600

    removed breaking backticks code rabbit added

--- ios/Sources/CapgoCameraPreviewPlugin/CameraController.swift
+++ ios/Sources/CapgoCameraPreviewPlugin/CameraController.swift
@@ -484,6 +484,7 @@ extension CameraController {
                 self.setInitialZoom(level: initialZoomLevel)
 
                 // Set up listener for change in subject area of camera feed
+                NotificationCenter.default.removeObserver(self, name: .AVCaptureDeviceSubjectAreaDidChange, object: nil)
                 NotificationCenter.default.addObserver(self, selector: #selector(self.subjectAreaDidChange), name: .AVCaptureDeviceSubjectAreaDidChange, object: nil)
 
                 // Start the session - all outputs are already configured
@@ -1866,7 +1867,7 @@ extension CameraController {
                     device.exposurePointOfInterest = point
                 }
             }
-            
+
             // Turn on subject area monitor for switch to continuous focus if needed
             device.isSubjectAreaChangeMonitoringEnabled = true
 
